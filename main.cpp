b18361c16e1103c4ce934a4d4f5a57018b837985
Adhish Majumdar <adhishm@gmail.com>
2015-10-03 18:04:59 +0200

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,21 @@
+/* 
+ * File:   main.cpp
+ * Author: adhish
+ *
+ * Created on October 3, 2015, 5:56 PM
+ */
+
+#include <cstdlib>
+#include <iostream>
+
+#include "Leap.h"
+
+int main(int argc, char** argv)
+{
+    Leap::Controller controller;
+    
+    
+
+    return 0;
+}
+
